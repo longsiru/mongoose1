51ea71b1046438cba85d71c350fd40e5e2664033
longsiru <sirulong89@gmail.com>
2023-04-14 00:02:29 +0900

    setting

---
+++ app.js
@@ -0,0 +1,6 @@
+//1.npm init --yes   —生成package.json的配置文件
+//2.安装mongoose -- npm install mongoose --save
+//3.引用mongoose
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017//sayo");
