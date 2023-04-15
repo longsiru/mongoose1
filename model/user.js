1cee332fccb6dddf0288649ab9a1dd1856ced804
longsiru <sirulong89@gmail.com>
2023-04-15 23:42:03 +0900

    Update user.js

--- model/user.js
+++ model/user.js
@@ -19,3 +19,45 @@ var UserSchema = mongoose.Schema({
 var UserModel = mongoose.model("User", UserSchema, "user");
 //export
 module.exports = UserModel;
+
+var UserModel = require("./model/user.js");
+
+var user = new UserModel({
+  name: "赵四",
+  sn: "sn12232421433",
+  age: 30,
+  status: "error",
+});
+user.save(function (err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log("成功");
+});
+
+// UserModel.findBySn('123456781',function(){})
+
+/*
+        UserModel.findBySn('123456782',function(err,docs){
+
+            if(err){
+
+                console.log(err);
+
+                return;
+            }
+            console.log(docs)
+        })
+    */
+
+/*
+        var user = new UserModel({
+            name: '赵六',
+            sn:'123456781',
+            age: 29
+        });
+
+
+        user.print();   //自定义的实例方法
+    */
